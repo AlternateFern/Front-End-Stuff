b9049ed0adac89a427bd29cee3ccee9511cffd57
Nerd Floppa <108718753+YellowFernn@users.noreply.github.com>
2022-07-05 15:00:00 +0700

    front end
    v

---
+++ dafa/info.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>RPL INFO</title>
+    <style>
+    .active {
+    background-color: rgb(213, 196, 15);
+    }
+
+    li a:hover {
+        background-color: rgb(86, 80, 12);
+    }
+
+    li a:hover:not(.active) {
+        background-color: #111;
+    }
+    </style>
+</head>
+<body>
+<ul>
+        <li><a href="default.php">Home</a></li>
+        <li style="float:right"><a class="active" href="info.php">Info</a></li>
+        <li><a href="HTML.php">HTML</a></li>
+        <li><a href="CSS.php">CSS</a></li>
+        <li><a href="PHP.php">PHP</a></li>
+        <li><a href="JAVA.php">JAVA</a></li>
+    </ul>
+</body>
+</html>
