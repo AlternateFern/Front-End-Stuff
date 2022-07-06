4cddc1c2d0e3f4fe0d0300fa54e1e49465537baa
Nerd Floppa <108718753+YellowFernn@users.noreply.github.com>
2022-07-06 14:54:35 +0700

    info update

--- dafa/info.php
+++ dafa/info.php
@@ -5,11 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>RPL INFO</title>
     <style>
     .active {
     background-color: rgb(213, 196, 15);
     }
+
+    
 
     li a:hover {
         background-color: rgb(86, 80, 12);
@@ -18,16 +21,112 @@
     li a:hover:not(.active) {
         background-color: #111;
     }
+
+    html, body {
+        margin:0px;
+    }
+
+    .blackcoloricon {
+        font-size: 22px;
+        color: black;
+        padding: 5px;
+    }
+
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        max-width: 305px;
+        margin: auto;
+        text-align: center;
+        font-family: Arial;
+        background-color: white;
+    }
+
+    /* profile */
+
+    .profile {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        max-width: 300;
+        margin: auto;
+        text-align: center;
+        font-family: Arial;
+    }
+
+    .title {
+        color: grey;
+        font-size: 18px;
+    }
+    
+    .schoolname {
+        color: #6f6f6f;
+        font: arial;
+        font-size: 18px;
+    }
+
+    a.button:hover, a:hover {
+        opacity: 0.7;
+    }
+
+    a.button {
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 8px;
+        text-align: center;
+        cursor: pointer;
+        width: 289px;
+        font-size: 18px;
+        color: black;
+        background-color: #f8eb01;
+    text-decoration: white;
+}
+
+    /* end of profile */
+
+    .bg {
+        background-image: linear-gradient(rgba(0, 0, 0, 0.985),rgba(0, 0, 0, 0.6)) , url(https://cdn.discordapp.com/attachments/744035173229789257/993736897782349824/typograph.jpg);
+        background-color: #111;
+        margin-top: -20px;
+        padding-bottom: 500px;
+    }
+
+    h2 {
+        text-align: center;
+        padding-top: 25px;
+        color: white;
+        font-family: Helvetica;
+        text-shadow: 0px 0px 10px #bfbfbf;
+    }
+
+
     </style>
 </head>
 <body>
-<ul>
+    <ul><b>
         <li><a href="default.php">Home</a></li>
         <li style="float:right"><a class="active" href="info.php">Info</a></li>
         <li><a href="HTML.php">HTML</a></li>
         <li><a href="CSS.php">CSS</a></li>
         <li><a href="PHP.php">PHP</a></li>
         <li><a href="JAVA.php">JAVA</a></li>
-    </ul>
+    </b></ul>
+    
+    <div class="bg">
+    <h2>Website Created By :</h2>
+    <div class="card">
+        <img src="https://cdn.discordapp.com/avatars/543985691336507402/1b39041f55cea801167956dd4d95207d.png?size=4096">
+        <h1>M. Daffa Azriel .F</h1>
+        <p class="title">Student</p>
+        <p class="schoolname">SMK TI Muhammadiyah</p>
+        <div style="margin: 24px 0;">
+        <a class="blackcoloricon" href="https://www.facebook.com/profile.php?id=100072579914490" target="_blank"><i class="fa fa-facebook"></i></a>
+        <a class="blackcoloricon" href="https://twitter.com/zFernn_n" target=_blank"><i class="fa fa-twitter"></i></a>
+        <a class="blackcoloricon" href="https://www.youtube.com/channel/UCF30Xi9HBdclGLG5ca3H4hg" target=_blank"><i class="fa fa-youtube"></i></a>
+        <a class="blackcoloricon" href="https://www.instagram.com/zfernn_/" target="_blank"><i class="fa fa-instagram"></i></a>
+        
+        <b><p><a href="https://wa.me/6283178955441" target="_blank" class="button">Contact Us</a></p></b>
+        
+            </div>
+        </div>
+</div>
 </body>
 </html>
