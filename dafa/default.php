2943165c8176664e51660dd8babe4b62ec645506
Nerd Floppa <108718753+YellowFernn@users.noreply.github.com>
2022-07-06 14:53:44 +0700

    several noticeable changes

--- dafa/default.php
+++ dafa/default.php
@@ -29,17 +29,8 @@
         font-family: Arial, Helvetica, sans-serif;
     }
 
-    .darkmodenav {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-family: Arial, Helvetica, sans-serif;
-    }
 
-    /*rgb color or smth*/
-    .rgb {
+    .rpl {
     background: linear-gradient(to right, #bfbfbf, #b3b3b3 , #a6a6a6, #999999, #8c8c8c);
     -webkit-background-clip: text;
     background-clip: text;
@@ -47,6 +38,7 @@
     animation: rainbow_animation 6s ease-in-out infinite;
     background-size: 400% 100%;
     padding-top: 30px;
+    text-shadow: 0px 0px 10px #bfbfbf;
     }
 
     @keyframes rainbow_animation {
@@ -57,16 +49,6 @@
     50% {
         background-position: 100% 0;
     }
-    }
-
-    .dark-mode {
-        background-color: #111;
-        color: white
-    }
-
-    .vertical {
-        writing-mode : vertical-rl;
-        font-family: Arial, Helvetica, sans-serif;
     }
 
 
@@ -82,10 +64,11 @@
 
     * {
         text-align: center;
-        color:white
+
     }
 
     .dark_rgb {
+        text-shadow: 0px 0px 10px #bfbfbf;
     background: linear-gradient(to right, #023030, #023106 , #555705, #643806, #5f0404);
     -webkit-background-clip: text;
     background-clip: text;
@@ -113,7 +96,7 @@
     .bg {
         background-image: linear-gradient(rgba(0, 0, 0, 0.985),rgba(0, 0, 0, 0.6)) , url(https://cdn.discordapp.com/attachments/744035173229789257/993736897782349824/typograph.jpg);
         background-color: #111;
-        padding-bottom: 500px;
+        padding-bottom: 800px;
 
     }
 
@@ -122,25 +105,14 @@
     }
 
     #bg2 {
-        background-color: #ededd8;
+        background-color: #e7e7c9;
         border-radius: 25px;
         width: 1200px;
-        height: 900px;
+        height: 3000px;
         margin: auto;
         height: 50hv;
-        animation: animate 5s ease infinite;
-    }
+        padding-right : 10px;
 
-    @keyframes animate {
-        0%{
-            background-color: gold;
-        }
-        50%{
-            background-color: #fbff53;
-        }
-        100%{
-            background-color: gold;
-        }
     }
     
     .leftalign {
@@ -150,28 +122,67 @@
         font-size: larger;
     }
 
+    .align {
+        text-align: center;
+        width: 1000px;
+        height: 400px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+
+    b {
+        font-family: Arial;
+    }
+
     </style>
 </head>
 <body>
-
     <ul><b>
-        <li><a class="active" href="default.php">Home</a></li>
-        <li style="float:right"><a href="info.php">Info</a></li>
-        <li><a href="HTML.php">HTML</a></li>
-        <li><a href="CSS.php">CSS</a></li>
-        <li><a href="PHP.php">PHP</a></li>
-        <li><a href="JAVA.php">JAVA</a></li>
-    </b></ul>
+            <li><a class="active" href="default.php">Home</a></li>
+            <li style="float:right"><a href="info.php">Info</a></li>
+            <li><a href="HTML.php">HTML</a></li>
+            <li><a href="CSS.php">CSS</a></li>
+            <li><a href="PHP.php">PHP</a></li>
+            <li><a href="JAVA.php">JAVA</a></li>
+        </b></ul>
                     <div class="bg">
                   <b><div class="header">WELCOME</div></b>
                 <div id="bg2">
-            <h1 class="rgb">RPL / PPLG</h1>
+            <h1 class="rpl">RPL / PPLG</h1>
         <b class="dark_rgb">Rekayasa Perangkat Lunak / Pengembangan Perangkat Lunak dan Gim</b>
     <p style="color: black" class="leftalign">
 <b style="color: black">Rekayasa Perangkat Lunak (RPL, atau dalam bahasa Inggris: Software Engineering atau SE)</b> adalah satu bidang profesi yang mendalami cara-cara pengembangan perangkat lunak termasuk pembuatan, pemeliharaan, manajemen organisasi pengembangan perangkat lunak dan manajemen kualitas.
 </p>
-    <img src="https://www.projectcubicle.com/wp-content/uploads/2022/05/types-of-software.png-1.webp" style="width:1200px;height:500px;">
+    <img class="align" src="https://www.projectcubicle.com/wp-content/uploads/2022/05/types-of-software.png-1.webp";>
+    <br>
     <i style="color: black">source : https://www.projectcubicle.com/5-things-every-business-needs-in-order-to-become-a-success/types-of-software-png-1/</i>
+
+        <h1 class="rpl">Sejarah RPL</h1>
+<p class="leftalign"><b style="color: black"> Istilah <i style="color: black">software engineering</i>, pertama kali digunakan pada akhir tahun 1950-an dan sekitar awal 1960-an. Pada tahun 1968, <b style="color: cyan">NATO</b> menyelenggarakan konferensi tentang software engineering di Jerman dan kemudian dilanjutkan pada tahun 1969. Meski penggunaan kata software engineering masukan konferensi tersebut menimbulkan debat tajam tentang aspek engineering dari pengembangan perangkat lunak, banyak pihak yang menganggap konferensi tersebutlah yang menjadi awal tumbuhnya profesi rekayasa perangkat lunak
+                </b></p>
+                <br>
+                <p class="leftalign" style="font-weight: 1000;"><b style="font-size: 30px;">1945 - 1965: Awal</b></p>
+                <hr>
+                <p class="leftalign"><b style="color: black"> Istilah <i>software engineering</i> digunakan pertama kali pada akhir 1950-an dan awal 1960-an. Saat itu, masih terdapat debat tajam mengenai aspek engineering dari pengembangan perangkat lunak.
+<br>
+<br>
+Pada tahun 1968 dan 1969, komite sains NATO mensponsori dua konferensi tentang rekayasa perangkat lunak, yang memberikan dampak kuat terhadap perkembangan rekayasa perangkat lunak. Banyak yang menganggap bahwa dua konferensi inilah yang menandai awal resmi profesi rekayasa perangkat lunak. jangan pernah menganggap kalau software itu akn menjadi yang terbaik karena itu adalah sebuah karya yang bersifat sementara.
+                </b></p>
+                <p class="leftalign" style="font-weight: 1000;"><b style="font-size: 30px;">1965 - 1985: krisis perangkat lunak</b></p>
+                <hr>
+                <p class="leftalign"><b style="color: black"> Pada tahun 1960-an hingga 1980-an, banyak masalah yang ditemukan para praktisi pengembangan perangkat lunak. Banyak projek yang gagal, hingga masa ini disebut sebagai <b style="color: red">krisis perangkat lunak</b>. Kasus kegagalan pengembangan perangkat lunak terjadi mulai dari projek yang melebihi anggaran, hingga kasus yang mengakibatkan kerusakan fisik dan kematian. Salah satu kasus yang terkenal antara lain meledaknya roket Ariane akibat kegagalan perangkat lunak.</b></p>
+
+                <i>source: https://id.wikipedia.org/wiki/Sejarah_rekayasa_perangkat_lunak</i>
+                <br>
+                <p class="leftalign" style="font-weight: 1000;"><b style="font-size: 30px;">Tragedi Roket Ariane 5</b></p>
+                <img class="align" src="https://media.discordapp.net/attachments/744035173229789257/994140978808098856/unknown.png";>
+                <p class="leftalign"><b style="color: black"> Ariane 5 adalah roket kelas berat yang dikembangkan oleh 10 negara Eropa. Roket ini dalam penyempurnaannya jadi roket andalan yang masih digunakan sampai sekarang.
+                <br>
+                <br>
+                Di perkembangan awalnya, roket Ariane 5 ini pertamakali dilontarkan pada 4 Juni 1996. Namun permasalahannya, dengan perkembangan roket dengan tingkatan jauh lebih besar dan kuat, komputer internal dan software yang menanganinya, hanya menggunakan software lawas milik Ariane 4.
+                <br>
+                <br>
+                Ketika kecepatan melebihi kemampuan komputer, angka yang terkomputerisasi akhirnya bergeser dan membuat roket berbelok tiba-tiba. Akhirnya terjadi ledakan udara dan roket hancur di angkasa. Segala muatannya pun ikut hancur.</b></p>
+                <i>source: https://www.merdeka.com/teknologi/6-kesalahan-komputer-terfatal-sepanjang-sejarah.html</i>
             </div>
         </div>
     </body>
